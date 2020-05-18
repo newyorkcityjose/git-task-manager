4a7b4461c3d9af3408663f890ca1bcc5268dda36
Jose Tavarez <newyorkcityjose@gmail.com>
2020-05-17 21:36:10 -0400

    Updates

---
+++ precache-manifest.8183fac3e89e45bfebc360c4f0991c44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdbb3e6ada9d1369654c4a5edb962f3c",
+    "url": "/git-task-manager/index.html"
+  },
+  {
+    "revision": "f8020b1292635a9ddd11",
+    "url": "/git-task-manager/static/css/main.8eb78f1c.chunk.css"
+  },
+  {
+    "revision": "9a1a4159f03f881ffa8a",
+    "url": "/git-task-manager/static/js/2.08ba0362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git-task-manager/static/js/2.08ba0362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8020b1292635a9ddd11",
+    "url": "/git-task-manager/static/js/main.c4cec3ca.chunk.js"
+  },
+  {
+    "revision": "e2749bb037a4c0bd1985",
+    "url": "/git-task-manager/static/js/runtime-main.f9c3f7f0.js"
+  }
+]);
